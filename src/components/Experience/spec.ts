963f12bef62a5415b8ce185ad38c946fd20d3d54
Stan <tonytonitone6@gmail.com>
2024-08-21 17:53:03 -0400

    fix: adjust working experience

--- src/components/Experience/spec.ts
+++ src/components/Experience/spec.ts
@@ -3,40 +3,40 @@ import type { JobDescriptionType } from './types';
 export const experienceList: JobDescriptionType[] = [
   {
     name: 'Chunghwa Telecom',
-    jobTitle: 'Senior Front-End Developer',
+    jobTitle: 'Senior Frontend Developer',
     location: 'Taipei City, TW',
     date: {
       from: 'September 2022',
       to: 'March 2024',
     },
     descList: [
-      'Implemented new features in our electric vehicle such as login module which support social login, and tree view selection module is easier to choose organize structure.',
-      'Created Media Module, which interface of upload includes preview our video files and image files.',
-      'Customized digital signage layout module using vanilla javascript.',
-      'Using storybook Incorporation into our whole project makes it easier to maintain various components.',
-      'In charge of department main reviewer to improve remembers code productivity and code quality.',
-      'As project leader takes a colleague to implement app for the Thailand government, which were used to monitor trash bin and pole light status.',
+      'We implemented new features in our electric vehicle, such as a login module and a tree view selection module, which makes it easier to choose an organized structure.',
+      'Created Media Module, which upload interface includes a preview of our video and image files, and Created a customized video player for the user.',
+      'Using React Native to build weather and info of app to provide citizens.',
+      'Integrating storybook and unit test made the UI designer and frontend developer check the codebase robust and find bugs easier, with coverage from 0% to 10% in the codebase.',
+      'Customized digital signage layout module using vanilla JavaScript.',
+      "In charge of the department's primary reviewer to improve code productivity and code quality.",
+      'Caching unveils data to increase user experience to avoid response time causing issues.',
     ],
   },
   {
     name: 'imbee',
-    jobTitle: 'Front-End Developer',
+    jobTitle: 'Frontend Developer',
     location: 'Hong Kong, HK, REMOTE',
     date: {
       from: 'November 2021',
       to: 'September 2022',
     },
     descList: [
-      'Sync chat content(ex. text, image, video) between public chat room and private chat room',
-      'Developed common module',
-      'Reporting services that provides data collection as visual diagram to client',
-      'Content group module',
-      'Customized count down force logout module by user setting',
+      'Sync chat content(e.g., text, image, video) between the public chat and the private chat.',
+      'Developed a common ui module.',
+      'Reporting services that provide data collection as visual diagrams to clients.',
+      'Customized count-down force logout module by the user setting.',
     ],
   },
   {
     name: 'Sinopac Security',
-    jobTitle: 'Senior Front-End Engineer',
+    jobTitle: 'Senior Frontend Engineer',
     location: 'Taipei City, TW',
     date: {
       from: 'April 2021',
@@ -44,21 +44,21 @@ export const experienceList: JobDescriptionType[] = [
     },
     descList: [
       'Build inviteCode and activityCode interface to show information visualization.',
-      'Build multiProcessor deal with webSocket for sub-brokerage api',
+      'Build multiProcessor deal with webSocket for sub-brokerage api.',
     ],
   },
   {
     name: 'Anue',
-    jobTitle: 'Front-End Developer',
+    jobTitle: 'Frontend Developer',
     location: 'Taipei City, TW',
     date: {
       from: 'March 2020',
       to: 'March 2021',
     },
     descList: [
-      'Created shared modules using context pattern and wrote a unit test',
-      'For the specific view need to realtime updated, design a pattern to reduce 10% call api frequency',
-      'Refactor code base of landing page and build campaign page',
+      'Created shared modules using compound pattern and wrote a unit test.',
+      'For the specific view need to realtime updated, design a pattern to reduce 10% call api frequency.',
+      'Refactor code base of landing page and build campaign page.',
     ],
   },
   {
@@ -70,9 +70,9 @@ export const experienceList: JobDescriptionType[] = [
       to: 'February 2020',
     },
     descList: [
-      'Built a content management system chatbot for news updates',
-      'Designed a News content management system for reporters to upload their press releases',
-      'Designed a new website using Progressive Web App for testing',
+      'Built a content management system chatbot for news updates.',
+      'Designed a News content management system for reporters to upload their press releases.',
+      'Designed a new website using Progressive Web App for testing.',
     ],
   },
   {
@@ -84,8 +84,8 @@ export const experienceList: JobDescriptionType[] = [
       to: 'May 2017',
     },
     descList: [
-      'Built a Inventory Management System for operators',
-      'Designed a customer reports interface for specific customers',
+      'Built a Inventory Management System for operators.',
+      'Designed a customer reports interface for specific customers.',
     ],
   },
 ];
